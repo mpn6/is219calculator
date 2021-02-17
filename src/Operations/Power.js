bece9e22e33ea2eb500b51a5fce11866f1243c70
Michelle Nunez <mpnunez5@gmail.com>
2021-02-17 18:39:55 -0500

    added the operation files: sum, difference, product, quotient, root, and power

---
+++ src/Operations/Power.js
@@ -0,0 +1,5 @@
+function Power(a, b){
+    return Math.pow(a,b);
+    //raises a number (a) to the power of a second number (b)
+}
+module.exports = Power;
---
+++ src/Operations/Root.js
@@ -0,0 +1,5 @@
+function Root(a, b){
+    return Math.pow(a, 1/b);
+    //takes the root of a number (a) by raising it to the power of 1 over the second number (b)
+}
+module.exports = Root;
